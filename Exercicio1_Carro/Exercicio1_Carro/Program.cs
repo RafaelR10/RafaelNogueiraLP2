e7368a65b158c2dcf2eb29ee717a26527fc7bc37
Sarah <sarahborretti@gmail.com>
2018-06-28 12:07:58 -0300

    ETAPA 2

--- Exercicio1_Carro/Exercicio1_Carro/Program.cs
+++ Exercicio1_Carro/Exercicio1_Carro/Program.cs
@@ -10,45 +10,58 @@ namespace Exercicio1_Carro
     {
         static void Main(string[] args)
         {
-            string modelo;
+          string modelo;
             double quilometragem;
             int potencia;
+            int qtd;
+            
+            Console.Write("Insira a quantidade de veículos");
+            qtd = int.Parse(Console.ReadLine());
 
-            Console.Write("Insira o modelo do veículo");
-            modelo = Console.ReadLine();
-            Console.Write("Insira a quilometragem do veículo");
-            quilometragem = double.Parse(Console.ReadLine());
-            Console.Write("Insira a potencia do veículo");
-            potencia = int.Parse(Console.ReadLine());
+            for (int i = 0; i < qtd; i++)
+            {
 
-            Console.Write("{0} - ", modelo);
 
-            if (quilometragem > 25000)
-            {
-                Console.Write("velho -");
-            }
-            else if (quilometragem <= 500)
-            {
-                Console.Write("Novo -");
-            }
-            else
-            {
-                Console.Write("seminovo - ");
-            }
+                Console.Write("Insira o modelo do veículo");
+                modelo = Console.ReadLine();
+                Console.Write("Insira a quilometragem do veículo");
+                quilometragem = double.Parse(Console.ReadLine());
+                Console.Write("Insira a potencia do veículo");
+                potencia = int.Parse(Console.ReadLine());
 
-            if (potencia > 170)
-            {
-                Console.WriteLine("Potente");
-            }
+                Console.Write("{0} - ", modelo);
 
-            else if (potencia < 90)
-            {
-                Console.WriteLine("Popular");
-            }
-            else
-            {
-                Console.WriteLine("Médio");
+                if (quilometragem > 25000)
+                {
+                    Console.Write("velho -");
+                }
+                else if (quilometragem <= 500)
+                {
+                    Console.Write("Novo -");
+                }
+                else
+                {
+                    Console.Write("seminovo - ");
+                }
+
+                if (potencia > 170)
+                {
+                    Console.WriteLine("Potente");
+                }
+
+                else if (potencia < 90)
+                {
+                    Console.WriteLine("Popular");
+                }
+                else
+                {
+                    Console.WriteLine("Médio");
+                }
+
             }
         }
     }
 }
+        
+   
+
